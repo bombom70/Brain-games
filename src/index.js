8a14cb165d76201654e0a5ac43fb4b462b0ed1bd
bombom <khamdamov_1996@mail.ru>
2018-10-01 22:30:40 +0500

    second commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greetingUser = (name) => {
+	console.log("Welcome to the Brain Games!\n");
+	const userName = readlineSync.question('May I have your name? ');
+	console.log("Hello, " + userName + "!");
+};
+
+export default greetingUser;
