1afe5a2c49555bff8afc6fbb58d126fa3e2e210c
bombom <khamdamov_1996@mail.ru>
2018-10-03 10:26:08 +0500

    mini fix

--- src/index.js
+++ src/index.js
@@ -11,29 +11,29 @@ export const greetingUser = () => {
 };
 
 const isEven = n => n % 2 === 0;
-let counter = 3;
 
-const evenNumber = (name, counter) => {
-  if (counter === 0) {
-    console.log(`Congratulations, ${name}!`);
-    return;
-  }
-  const randomNum = Math.floor(Math.random() * 100);
+const evenNumber = (name) => {
+  const iter = (counter) => {
+    if (counter === 3) {
+      console.log(`Congratulations, ${name}!`);
+      return;
+    }
+    const randomNum = Math.floor(Math.random() * 100);
 
-  console.log(`Question: ${randomNum}`);
+    console.log(`Question: ${randomNum}`);
 
-  const yoursAnswer = readlineSync.question(`Your answer: `);
+    const yoursAnswer = readlineSync.question(`Your answer: `);
 
-  const goodAnswer = isEven(randomNum) ? 'yes' : 'no';
+    const goodAnswer = isEven(randomNum) ? 'yes' : 'no';
 
-  const counter = 3;
+    if (yoursAnswer !== goodAnswer) {
+      console.log(`${yoursAnswer} is wrong answer ;(. Correct answer was ${goodAnswer}.\nLet's try again, {name}!`);
+      return;
+    }
+    return iter(counter + 1);
+  };
+  return iter(0);
 
-  if (yoursAnswer !== goodAnswer) {
-    console.log(`${yoursAnswer} is wrong answer ;(. Correct answer was ${goodAnswer}.\nLet's try again, {name}!`);
-    return;
-  }
-
-  return evenNumber(name, counter - 1);
 };
 
 export const evenGame = () => {
